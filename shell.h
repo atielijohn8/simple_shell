dc95c40a3975960502d003ac57626da6cbb4d65d
Lucianjoki <luciawaweru22@gmail.com>
2023-10-10 12:23:39 -0700

    shell.h

---
+++ shell.h
@@ -0,0 +1,19 @@
+<shell.h>
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+extern char **environ;
+void variableHandler(char **argv);
+
+#endif
