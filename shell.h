839c11d8235669f1cf299379bee9aac07b655390
atielijohn8 <atielijohn4@gmail.com>
2023-10-18 02:37:30 +0300

    header

--- shell.h
+++ shell.h
@@ -13,16 +13,29 @@
 #include <sys/types.h>
 
 extern char **environ;
-/**
- * @variableHandler: command that handles variable inputs
- * @*get_Path: get full path of the current dir
- * @ run_Cmd: take user input and runs
- * @stat: checks file status
- */
 
-void variableHandler(char **av);
+/**
+ * get_Path - get full path of the current dir
+ * @command:  comd to get the full path of
+ * directory
+ * Return: fullpath of cmd
+ */
 char *get_Path(char *command);
+
+/**
+ * run_Cmd - take input from user
+ * run it
+ * @av: string array represrnting cmd and its arguments
+ */
 void run_Cmd(char **av);
+
+/**
+ * stat - file status
+ * @pathname: file path
+ * @statbuf: struct pointer
+ * where struct will be stored
+ * Return: (0)success, (-1) failure
+ */
 int stat(const char *pathname, struct stat *statbuf);
 
 #endif
